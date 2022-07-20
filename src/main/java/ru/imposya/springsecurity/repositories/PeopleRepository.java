3ef4cc80bb65bfa1f8e45f2225607192727698b5
Alexander Latyshev <imposya0@gmail.com>
2022-07-20 14:17:29 +0300

    Initial commit

---
+++ src/main/java/ru/imposya/springsecurity/repositories/PeopleRepository.java
@@ -0,0 +1,13 @@
+package ru.imposya.springsecurity.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.imposya.springsecurity.models.Person;
+
+import java.util.Optional;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    Optional<Person> findByUsername(String username);
+}
