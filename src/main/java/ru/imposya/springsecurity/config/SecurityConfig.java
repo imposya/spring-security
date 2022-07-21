5b4116f122209cd8f9ae2ea7d3fbd28059e4624d
Alexander Latyshev <imposya0@gmail.com>
2022-07-21 16:29:33 +0300

    @PreAuthorize

--- src/main/java/ru/imposya/springsecurity/config/SecurityConfig.java
+++ src/main/java/ru/imposya/springsecurity/config/SecurityConfig.java
@@ -4,15 +4,16 @@ package ru.imposya.springsecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.imposya.springsecurity.services.PersonDetailsService;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PersonDetailsService personDetailsService;
@@ -31,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/admin").hasRole("ADMIN")
+        http.authorizeRequests()
                 .antMatchers("/auth/login", "/error", "/auth/registration").permitAll()
                 .anyRequest().hasAnyRole("USER", "ADMIN")
                 .and()
--- src/main/java/ru/imposya/springsecurity/controllers/HelloController.java
+++ src/main/java/ru/imposya/springsecurity/controllers/HelloController.java
@@ -1,14 +1,23 @@
 package ru.imposya.springsecurity.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.imposya.springsecurity.security.PersonDetails;
+import ru.imposya.springsecurity.services.AdminService;
 
 @Controller
 public class HelloController {
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
         return "hello";
@@ -24,6 +33,7 @@ public class HelloController {
     }
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
